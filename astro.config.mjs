d7991d75a7847c85899f91bc2a8cd948910ffb6f
bryan <bryanchu10@gmail.com>
2025-01-06 00:36:54 +0800

    Add Vue integration and Greeting component for dynamic messages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://blog.hosaki.net"
+  site: "https://blog.hosaki.net",
+  integrations: [vue()],
 });
---
+++ src/components/Greeting.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+const props = defineProps({
+  messages: Array
+})
+const greeting = ref(props.messages[0])
+const setGreeting = (message) => {
+  greeting.value = message
+}
+const randomMessage = () => props.messages[(Math.floor(Math.random() * props.messages.length))];
+</script>
+<template>
+  <div>
+    <h3>{{ greeting }}! Thank you for visiting!</h3>
+      <button @click="setGreeting(randomMessage())">
+        New Greeting
+      </button>
+  </div>
+</template>
+<style scoped></style>
