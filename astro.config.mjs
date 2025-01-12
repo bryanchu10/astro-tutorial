67ca6b3ef6e86108b688fa0fab28adb13abbf9c7
bryan <bryanchu10@gmail.com>
2025-01-12 23:42:58 +0800

    Add Tailwind CSS integration and update styles for blog layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,17 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.hosaki.net",
-  integrations: [vue()],
+  integrations: [
+    vue(),
+    tailwind({
+      applyBaseStyles: false,
+    })
+  ],
   i18n: {
     locales: ['en', 'zh-TW'],
     defaultLocale: 'zh-TW',
