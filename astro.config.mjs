828e446b28592e95116ebc4820a19f5b584fadcb
bryan <bryanchu10@gmail.com>
2025-01-15 22:26:21 +0800

    Add React components and Tailwind CSS configuration; refactor styles and utilities

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,14 @@ import vue from '@astrojs/vue';
 
 import tailwind from '@astrojs/tailwind';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.hosaki.net",
-  integrations: [
-    vue(),
-    tailwind({
-      applyBaseStyles: false,
-    })
-  ],
+  integrations: [vue(), tailwind({
+    applyBaseStyles: false,
+  }), react()],
   i18n: {
     locales: ['en', 'zh-TW'],
     defaultLocale: 'zh-TW',
