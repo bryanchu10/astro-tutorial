f08bd6681768347b963fc9ca8e6b406eeca62f2e
bryan <bryanchu10@gmail.com>
2025-01-27 18:49:21 +0800

    Add last modified date feature to blog posts; integrate dayjs for date formatting and update index page to display last modified dates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { remarkModifiedTime } from './remark-modified-time.mjs';
+
 import vue from '@astrojs/vue';
 
 import tailwind from '@astrojs/tailwind';
@@ -9,11 +11,20 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.hosaki.net",
-  integrations: [vue(), tailwind({
-    applyBaseStyles: false,
-  }), react()],
+  integrations: [
+    vue(),
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
   i18n: {
     locales: ['en', 'zh-TW'],
     defaultLocale: 'zh-TW',
-  }
+  },
+  markdown: {
+    remarkPlugins: [
+      remarkModifiedTime,
+    ],
+  },
 });
